2a6b1b3af18bd2c78d8d38d626506348a2551567
Mark <markrensen@hotmail.com>
2020-05-13 15:22:54 +0200

    added resize_to_screen and did some tuning to add_folder_button

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import cv2
 import PIL.Image, PIL.ImageTk
 from tkinter import messagebox
 from tkinter import simpledialog
+import getpass
 
 class FolderButton():
     def __init__(self, master, text, event):
@@ -17,21 +18,45 @@ class FolderButton():
         self.event = event
         self.Button = tk.Button(master, text=text, anchor=tk.SE, command=event)
 
+    def pack(self):
+        self.Button.pack()
+
+
+def resize_to_screen(img, masterw, masterh):
+    # this function resizes the image to fit the master window
+    w = img.width
+    h = img.height
+    neww = 0
+    newh = 0
+    ratio = w/h
+    if w>h :
+        neww = masterw
+        newh = round(neww/ratio)
+        return img.resize((neww, newh))
+    else:
+        newh = masterh
+        neww = round(newh * ratio)
+        return img.resize((neww, newh))
+
 
 
 class GUI(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        w,h = 650, 650
-        master.minsize(width=w, height=h)
-        master.maxsize(width=w, height=h)
+        self.w,self.h = 650, 650
+        self.buttonHeightOffset = 30
+        master.minsize(width=self.w, height=self.h)
+        master.maxsize(width=self.w, height=self.h)
 
+        self.base_path = "/home/"+getpass.getuser()+"/Pictures/PhotoSorter"
+        if not os.path.isdir(self.base_path): #make sure basepath exists, else make it
+            os.mkdir(self.base_path)
         self.photo_counter = 0
         self.extensions = ['.jpg', '.jpeg', '.img', '.png']
         self.photos = []
         self.folderButton_list = []  # list of folders the user can choose between to export the photo to
 
-        self.container = tk.Canvas(self, width=650, height=620)
+        self.container = tk.Canvas(self, width=self.w, height=self.h-self.buttonHeightOffset)
         self.buttonFrame = tk.Frame(master)
 
         #packs
@@ -46,29 +71,42 @@ class GUI(tk.Frame):
 
         self.get_photos()
 
-        self.container.create_image(20, 20, anchor=tk.CENTER, image=self.photos[self.photo_counter])
+        self.container.create_image(self.w/2, self.h/2, image=self.photos[self.photo_counter])
 
     def folderButton_click(self):
         return
 
     def add_folder_button(self):
-        # user wants to add a new folder to export to. Check if folder allready exists on drive, else make new folder.
-        answer = tk.messagebox.askquestion("new folder", "Do you want to create a new folder? Select 'no' if you want to use an existing folder")
+        # user wants to add a new folder to export to. Check if folder already exists on drive, else make new folder.
+        answer = tk.messagebox.askquestion("new folder", "Do you want to create a new folder? \n Select 'no' if you want to use an existing folder")
         folder = ""
+        text = ""
         if answer == "yes":
             #TODO:create new folder
-            folder = True
+            text = folder = tk.simpledialog.askstring("Folder name", "What would you like your new folder called?")
+            try:
+                os.mkdir(self.base_path + "/" + folder)
+            except FileExistsError:
+                tk.messagebox.showwarning('', 'Folder already exists')
+                self.add_folder_button()
+                return
         if answer == "no":
             #select folder
-            folder = filedialog.askdirectory()
-        text = tk.simpledialog.askstring(title="name", prompt="what is the name of this folder?")
+            #TODO: see if askdirectory() can be initiated from a different location, aka base_path
+            folder = filedialog.askdirectory(initialdir=self.base_path)
+            folder_tuple = folder.split()
+            text = folder_tuple[1]
+        #text = tk.simpledialog.askstring(title="name", prompt="what is the name of this folder?")
         button = FolderButton(self.buttonFrame, text, self.folderButton_click)
-        if folder in self.folderButton_list:
+        if button in self.folderButton_list:
             tk.messagebox.showwarning('', 'Folder already exists')
             self.add_folder_button()
             return
         else:
             self.folderButton_list.append(button)
+        print(self.folderButton_list)
+        button.pack()
+        self.buttonFrame.pack()
 
 
 
@@ -84,12 +122,17 @@ class GUI(tk.Frame):
                 for ext in self.extensions:
                     if ext in file:
                         img = self.create_tk_image_from_path(os.path.join(r, file))
-                        self.photos.append(img)
+
 
     def create_tk_image_from_path(self, file_path):
         # make the image from file_path TK-ready (tk doesn't read jpg) and load these images into photos[]
-        img = cv2.cvtColor(cv2.imread(file_path), cv2.COLOR_BGR2RGB)
-        image = PIL.ImageTk.PhotoImage(PIL.Image.fromarray(img))
+        image = PIL.Image.open(file_path)
+        print("old: {} + {}".format(image.width, image.height))
+        image = resize_to_screen(image, self.w, self.h-self.buttonHeightOffset) #this function resizes the image to fit the master window
+        print("new: {} + {}".format(image.width, image.height))
+        photoimage = PIL.ImageTk.PhotoImage(image)
+        self.photos.append(photoimage)
+
         return image
 
 
